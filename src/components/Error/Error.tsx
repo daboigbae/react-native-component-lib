162b296762df36aa94bc7e65c9c5ea09f4a72fb3
Manbeartrain <ahigareda15@gmail.com>
2022-11-07 20:35:10 -0700

    Text field component updates
    Added placeholderTextColor, value, and fixed styling on error message and text input parent container

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,34 +1,26 @@
-import React from "react"; 
+import React from "react";
 
-import { 
-	View,
-	Text,
-	StyleSheet
-} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 interface ErrorType {
 	error?: string;
 }
 
-const Error: React.FC<ErrorType> = ({
-	error
-}) => (
-		<View style={style.container}>
-			{error && <Text style={style.text}>
-				{error}</Text>}
-		</View>
-	)
+const Error: React.FC<ErrorType> = ({ error }) => (
+	<View style={style.container}>
+		{error && <Text style={style.text}>{error}</Text>}
+	</View>
+);
 
 const style = StyleSheet.create({
 	container: {
-		width: '100%', 
-		height: 12, 
-		marginTop: 4, 
+		width: "100%",
+		marginTop: 4,
 		marginLeft: 4
-	}, 
+	},
 	text: {
-		fontSize: 12, 
-		color: '#f00'
+		fontSize: 12,
+		color: "#f00"
 	}
-})
+});
 export default Error;
--- src/components/TextInput/TextInput.types.ts
+++ src/components/TextInput/TextInput.types.ts
@@ -1,15 +1,17 @@
 export interface FormInputType {
-	type?: 'email' | 'password' | 'text';
+	type?: "email" | "password" | "text";
 	label?: string;
 	error?: string;
+	value?: string;
 	onChangeText?: (text: string) => void;
+	placeholderTextColor?: string;
 	editable?: boolean;
 }
 
 export interface optionType {
 	label?: string;
 	placeholder?: string;
-	autoCapitalize?: 'none' | 'sentences' | 'words' | 'characters';
-	keyboardType?: 'default' | 'email-address' | 'numeric' | 'phone-pad';
+	autoCapitalize?: "none" | "sentences" | "words" | "characters";
+	keyboardType?: "default" | "email-address" | "numeric" | "phone-pad";
 	secureTextEntry?: boolean;
 }
